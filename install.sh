2f28d1e868aa3cf40b467d3fbedb0abdeeb3e887
David Fischer <djfische@gmail.com>
2013-06-08 17:55:17 -0700

    Install the python jedi submodule

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 DOTFILES=~/.dotfiles
 
-cd $DOTFILES && git submodule update --init
 rm -f ~/.bashrc && ln -s $DOTFILES/bashrc ~/.bashrc
 rm -f ~/.gitconfig && ln -s $DOTFILES/gitconfig ~/.gitconfig
 rm -f ~/.editorconfig && ln -s $DOTFILES/editorconfig ~/.editorconfig
 rm -f ~/.vimrc && ln -s $DOTFILES/vimrc ~/.vimrc
 rm -rf ~/.vim && ln -s $DOTFILES/vim ~/.vim
+
+cd $DOTFILES && git submodule update --init
+cd $DOTFILES/vim/bundle/jedi-vim && git submodule update --init
