4844c59d5f72dcff10ffce916d10ad820ef00191
David Fischer <djfische@gmail.com>
2019-11-07 11:02:05 -0800

    Add global gitignore file

--- install.sh
+++ install.sh
@@ -8,5 +8,7 @@ rm -f ~/.editorconfig && ln -s $DOTFILES/editorconfig ~/.editorconfig
 rm -f ~/.vimrc && ln -s $DOTFILES/vimrc ~/.vimrc
 rm -rf ~/.vim && ln -s $DOTFILES/vim ~/.vim
 
+mkdir -p ~/.config/git && rm -f ~/.config/git/ignore && ln -s $DOTFILES/git/ignore ~/.config/git/ignore
+
 cd $DOTFILES && git submodule update --init
 cd $DOTFILES/vim/bundle/jedi-vim && git submodule update --init
